3bb6e085d1cb503b855220de24d9335a0df91793
Lexxyla <bamideleadefolaju@gmail.com>
2022-03-10 02:36:23 -0800

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
- */
- int main(void)
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
