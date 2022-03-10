d1c494d2c92eb6352cd5c23ad5c69cb8ee63cfff
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-10 11:11:14 +0000

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
+#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <string.h>
 /**
  * main - Entry point
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str1, 59);
 	return (1);
 }
