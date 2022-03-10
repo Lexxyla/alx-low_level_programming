4383841ade50dcd59d5d84a1a90c7a60227297b0
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-10 11:17:32 +0000

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19\n";
-	write(2, str1, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
