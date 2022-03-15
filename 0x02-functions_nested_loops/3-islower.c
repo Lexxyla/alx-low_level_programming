fb0bca02b4e2d79f0aef8e6918ab52eb86ddedf3
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-15 16:26:33 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,15 +10,14 @@
  * Return: 1 for lowercase character. 0 for the rest.
  */
 int _islower(int c)
-{
-if (c >= 97 && c <= 122)
-{
-	return (1);
-}
-	
-else
-{
-	return (0);
-}
-	_putchar('\n');
-}
+	{
+		if (c >= 97 && c <= 122)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+		_putchar('\n');
+	}
