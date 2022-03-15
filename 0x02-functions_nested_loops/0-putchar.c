d41714c570fb5f41130876b164a739d243167b47
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-15 12:52:15 +0100

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * main -  prints Holberton, followed by a new line
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
