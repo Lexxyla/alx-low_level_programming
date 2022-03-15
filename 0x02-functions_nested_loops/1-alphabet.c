7d104c13702ce9d40ace38f432f0807d7f7d7df9
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-15 10:48:30 +0100

    Nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
+#include "holberton.h"
 
+/**
+ * print_alphabet -  prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
