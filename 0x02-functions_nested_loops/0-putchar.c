450a75adf699a41764a10b12f0177de75cfaad9c
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-15 12:50:18 +0100

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main -  prints Holberton, followed by a new line
@@ -6,16 +6,12 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
