2f88231cdb04582f3710b275745ccaf0795932c1
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-15 11:00:01 +0100

    Nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * int _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return 1;
+	else
+		return 0;
+}
