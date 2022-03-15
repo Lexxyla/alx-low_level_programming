e5cf09bb012324ca15c091da0f251b97d77cf462
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-15 16:18:02 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,13 +11,14 @@
  */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c >= 97 && c <= 122)
+{
+	return (1);
+}
+	
+else
+{
+	return (0);
+}
 	_putchar('\n');
 }
