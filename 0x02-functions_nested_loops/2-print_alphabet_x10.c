5b9939f93b02cb6cd29d91d95f3bd2dafd8d2045
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-15 15:32:16 +0100

    Nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,20 @@
  */
 void print_alphabet_x10(void)
 {
-        char ch;
-        int i;
+	char ch;
+	int i;
 
-        i = 0;
-        while (i < 10)
-        {
-                ch = 'a';
-                while (ch <= 'z')
-                {
-                        _putchar(ch);ch++;
-                }
-                _putchar('\n');i++;
-        }
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
