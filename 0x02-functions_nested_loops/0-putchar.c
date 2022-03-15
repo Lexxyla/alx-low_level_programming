dfad48681d6d0d5f75c4493aac5f8c7d93ee5573
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-15 11:54:55 +0100

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fputchar('p');
+	fputchar('u');
+	fputchar('t');
+	fputchar('c');
+	fputchar('h');
+	fputchar('a');
+	fputchar('r');
+
+	return (0);
+}
