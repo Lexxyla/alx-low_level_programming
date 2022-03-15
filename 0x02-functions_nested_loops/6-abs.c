2e4f1a6e974134d9bc21e3cddfdd5969a3a94535
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-15 11:05:28 +0100

    Nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	} else
+	{
+		return (-n);
+	}
+}
