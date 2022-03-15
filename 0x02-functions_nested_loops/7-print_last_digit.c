979ed743cc695d3540c6329a39f218a2d693fa67
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-15 11:21:22 +0100

    Nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	r = n % 10;
+
+	_putchar(r + '0');
+
+	return (r);
+}
