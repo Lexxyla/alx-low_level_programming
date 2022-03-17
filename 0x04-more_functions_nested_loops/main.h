20c82797228bed4e49c2e5f05fd1fa6cf323225d
Lexxyla <bamideleadefolaju@gmail.com>
2022-03-17 05:01:38 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char);
+int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
