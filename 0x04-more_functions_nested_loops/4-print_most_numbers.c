b4c5049add55d91c3ebc7c298e73bb6b0420d0fc
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-03-17 12:43:13 +0100

    more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * prints the numbers, from 0 to 9, followed by a new line
- * Do not print 2 and 4
- * Return: None
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
-	int ch;
-	for (ch = 48; ch < 58; ch++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
-		if (ch != 50 && ch != 52)
-			_putchar(ch);
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 	}
+
 	_putchar('\n');
 }
