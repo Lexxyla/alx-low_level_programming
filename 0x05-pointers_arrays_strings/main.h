a26c82f91bd67419af1e56f0cdd84667f9e31fa7
Lexxyla <bamideleadefolaju@gmail.com>
2022-03-21 02:58:29 -0700

    pointer_arrays_strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+int _putchar(char);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
