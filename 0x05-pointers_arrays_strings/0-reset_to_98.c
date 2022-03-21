f1aeb0144445e5943fe6e39685704225a2171643
Lexxyla <bamideleadefolaju@gmail.com>
2022-03-21 00:44:11 -0700

    pointers_array_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * takes a pointer to an int as parameter and updates the value it points to to 98
+ * @n: input interger.
+ * Return: no return.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
