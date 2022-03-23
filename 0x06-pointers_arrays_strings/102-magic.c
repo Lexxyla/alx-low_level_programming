035efeb8805e1962a3e7e83c4b711e73a0fd560d
Lexxyla <bamideleadefolaju@gmail.com>
2022-03-23 17:29:55 +0000

    pointers_arrays_strings2

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -15,9 +15,8 @@ int main(void)
 	 * - you are not allowed to modify p
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
+	p[5] = 98;
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
