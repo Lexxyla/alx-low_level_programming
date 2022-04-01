b866741234f56213c1f68e218340e7d3e83bb4b8
Lexxyla <87193747+Lexxyla@users.noreply.github.com>
2022-04-01 15:22:59 +0100

    static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,8 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+/*a list that was given from the task 9 */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +23,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
