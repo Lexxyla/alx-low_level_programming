87291b5b28d904d65db72b98177f0a3c20b67173
Lexxyla <bamideleadefolaju@gmail.com>
2022-04-07 09:06:00 +0000

    prototypes for files to run

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,3 +4,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
