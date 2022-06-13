1d33be16db2d63c5967d5bda0980d45ebcde32dc
Bamidele Adefolaju <87193747+Lexxyla@users.noreply.github.com>
2022-06-13 12:47:12 +0100

    Code Injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/lexxyla/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://github.com/Lexxyla/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so?raw=true
 export LD_PRELOAD=/tmp/nrandom.so
