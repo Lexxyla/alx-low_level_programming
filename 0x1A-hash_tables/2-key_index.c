2a851c4b39247de1b37280688e68a4fab0c96380
Lexxyla <bamideleadefolaju@gmail.com>
2022-07-01 10:42:43 +0000

    add key index function

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,8 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Get the index at which a key/value pair should be stored in array of a hash table.
+ * key_index - Get the index at which a key/value
+ *      pair should be stored in array of a hash table.
  * @key: The key to get the index of.
  * @size: The size of the array of the hash table.
  *
